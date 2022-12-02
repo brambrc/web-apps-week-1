e158675f05c3a6d0c738c7d5dcecaa020651586f
Anggiet Bracmatya <anggietbracmatya@192.168.1.10>
2022-12-03 01:06:52 +0700

    latest udpdate

--- client.go
+++ client.go
@@ -14,12 +14,6 @@ type Quotes struct {
 	QuoteHTML string `json:"h"`
 }
 
-type AnimeQuotes struct {
-	Anime string `json:"anime"`
-	Char  string `json:"character"`
-	Quote string `json:"quote"`
-}
-
 func getQuotes(w http.ResponseWriter, r *http.Request) {
 
 	// Statement yang menghasilkan instance http.Client, diperlukan untuk eksekusi request
@@ -72,57 +66,11 @@ func getQuotes(w http.ResponseWriter, r *http.Request) {
 }
 func getQuotesFromAnime(w http.ResponseWriter, r *http.Request) {
 	//silahkan bikin hall yang sama dengan endpoint /getQuotes tapi dengan endpoint dibawah ini
-	//https://animechan.vercel.app/api/random
-	// dokumentasi https://animechan.vercel.app/docs
-	// pilih api yang bisa mendapatkan 10 quotes random sekaligus, dan tampilkan semuanya
-
-	var client = &http.Client{}
-
-	// http.NewRequest() digunakan untuk membuat request baru
-	hitTheApis, err := http.NewRequest("GET", "https://animechan.vercel.app/api/quotes", nil)
-	if err != nil {
-		panic(err)
-	}
-
-	resp, err := client.Do(hitTheApis)
-	if err != nil {
-		panic(err)
-	}
-
-	// Cetak status code request
-	fmt.Println("Status: ", resp.Status)
-
-	// Kita bisa membaca response body menggunakan package ioutil.
-	responseData, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//disini response bodynya kita unmarshall dan convert ke struct Quotes
-	var quote []AnimeQuotes
-	var err2 = json.Unmarshal(responseData, &quote)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
-
-	//disini kita convert struct Quotes ke json
-
-	data := make([]AnimeQuotes, 0)
-	for i := 0; i < len(quote); i++ {
-		quo := &AnimeQuotes{
-			Anime: quote[i].Anime,
-			Char:  quote[i].Char,
-			Quote: quote[i].Quote,
-		}
-		_, err := json.Marshal(quo)
-		if err != nil {
-			fmt.Println(err)
-		}
-		data = append(data, *quo)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+	//dokumentasi https://animechan.vercel.app/docs
+	//pilih api yang bisa mendapatkan 10 quotes random sekaligus, dan tampilkan semuanya
+	//struktur functionnya sama dengan function getQuotes diatas, namun karena datanya lebih banyak, maka kita perlu menggunakan array of struct
+	//dan juga perlu menggunakan looping untuk menampilkan semua quotes
+	//hint: tambahkan struct baru yang sesuai dengan struktur json yang didapatkan dari api
 
 }
 
