0ead03a364a29dd3640974915ddc8efa0791167e
sarjanisahmanurung <sarjanisahmanurung22@gmail.com>
2022-12-08 10:21:02 +0700

    commit-1

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"anime_quotes/db"
+	"anime_quotes/model"
+	"anime_quotes/repository"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func anime_quotes() {
+	conn, err := db.ConnectToDB()
+
+	if err != nil {
+		panic(err)
+	}
+
+	conn.AutoMigrate(&model.QuotesAnime{})
+
+	q := repository.NewQuoteRepository(conn)
+
+	http.HandleFunc("/insert-quotes-from-api", q.InsertQuotesFromAPI)
+	http.HandleFunc("/get-quotes-from-api", q.GetQuotesFromDb)
+	http.HandleFunc("/get-total-quotes", q.GetTotalQuotes)
+	http.HandleFunc("/insert-quote", q.InsertQuotes)
+
+	log.Println("Listening...at port 3333")
+	err = http.ListenAndServe(":3333", nil)
+
+	if err != nil {
+		fmt.Printf("Error starting server: %s \n", err)
+		return
+	}
+
+}
